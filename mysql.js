75b4cfb2122e9077cba09a4d4f040db628dc62c9
unknown <you@example.com>
2019-11-21 14:04:05 -0500

    updated db test

--- mysql.js
+++ mysql.js
@@ -5,12 +5,17 @@ var con = mysql.createConnection({
     host: "dbtbt326.cjwujllbnqqi.us-east-1.rds.amazonaws.com",
     port: "3306",
     user: "mlehoullier",
-    password: "DBTBT326"
+    password: "DBTBT326",
+    database: "DBTBT326"
   });
   
   con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
+    var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("Table created");
+    });
   });
-
   
