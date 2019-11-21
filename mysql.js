cf75843c0848d3881c1c769c2c14547752033d9a
unknown <you@example.com>
2019-11-21 13:40:31 -0500

    add db Test

---
+++ mysql.js
@@ -0,0 +1,16 @@
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "dbtbt326.cjwujllbnqqi.us-east-1.rds.amazonaws.com",
+    port: "3306",
+    user: "mlehoullier",
+    password: "DBTBT326"
+  });
+  
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+
+  
