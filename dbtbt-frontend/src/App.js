63dfac02f39f2debfa079f8cf285ad97d0bd953c
AardeYamz <33364527+AardeYamz@users.noreply.github.com>
2019-11-27 16:12:22 -0500

    Basic Search Front Page

--- dbtbt-frontend/src/App.js
+++ dbtbt-frontend/src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MyFilteringComponent from './MyFilteringComponent';
 
 function App() {
+  //temporary hardcoded search data
+  let searchInput =
+    [
+    "Afghanistan",
+    "Åland Islands",
+    "Albania",
+    "Algeria",
+    "John"
+    ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MyFilteringComponent content={searchInput} />
   );
 }
 
