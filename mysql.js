cee3b383c3bf8102781a237502e64620cc37bcd7
unknown <you@example.com>
2019-11-29 15:13:42 -0500

    update mysql

--- mysql.js
+++ mysql.js
@@ -9,15 +9,6 @@ var con = mysql.createConnection({
     database: "mydb"
   });
   
-  // con.connect(function(err) {
-  //   if (err) throw err;
-  //   console.log("Connected!");
-  //   var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
-  //   con.query(sql, function (err, result) {
-  //     if (err) throw err;
-  //     console.log("Table created");
-  //   });
-  // });
 
   con.connect(function(err) {
     if (err) throw err;
