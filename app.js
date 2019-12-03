cbdb7a047e887dd0d5d03eb49bd1b848664028a1
cdaher <cdaher@umass.edu>
2019-12-03 15:51:24 -0500

    app.js fix

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const scraper = require("./scraperclientjson.js");
 let app = express();
 
-app.listen(3000, () => {
+app.listen(3001, () => {
   console.log("Server running on port 3000");
 });
 
